108141782533442b90cf8ce652687eff37ff6387
jerry7991 <jerry31097@gmail.com>
2021-05-28 12:34:27 +0530

    May2021

--- first.java
+++ first.java
@@ -1,10 +1,5 @@
-import  java.util.*;
-class first
-{
-  public static void main(String[] args) {
-    System.out.println("Enter number");
-    Scanner scan=new Scanner(System.in);
-    int n=scan.nextInt();
-    System.out.println(n);
-  }
+class First{
+    public static void main(String[] args) {
+        System.out.println("Hi");
+     }
 }
